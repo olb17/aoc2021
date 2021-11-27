38160dde9970b49100ca35bab1eeb3021da25172
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2021-11-27 08:11:43 +0000

    init

---
+++ test/advent_of_code/day_01_test.exs
@@ -0,0 +1,23 @@
+defmodule AdventOfCode.Day01Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Day01
+
+  @input "1 1396"
+
+  @tag :skip
+  test "part1" do
+    input = @input
+    result = part1(input)
+
+    assert result
+  end
+
+  @tag :skip
+  test "part2" do
+    input = @input
+    result = part2(input)
+
+    assert result
+  end
+end
