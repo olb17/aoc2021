994a2fdb6306da20c9c2fc17dbbda212d31fc47c
Olivier Belhomme <2453684+olb17@users.noreply.github.com>
2021-12-01 20:22:56 +0000

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,9 +1,36 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
-    args
+    {_, res} =
+      args
+      |> String.split()
+      |> Enum.map(fn i -> String.to_integer(i) end)
+      |> Enum.reduce({-1, 0}, fn i, {curr, nb} ->
+        if curr != -1 and i > curr do
+          {i, nb + 1}
+        else
+          {i, nb}
+        end
+      end)
+
+    res
   end
 
   def part2(args) do
-    args
+    [a | [b | list]] =
+      args
+      |> String.split()
+      |> Enum.map(fn i -> String.to_integer(i) end)
+
+    {_, res} =
+      Enum.zip([list, [b | list], [a | [b | list]]])
+      |> Enum.reduce({-1, 0}, fn {a, b, c}, {curr, nb} ->
+        if curr != -1 and a + b + c > curr do
+          {a + b + c, nb + 1}
+        else
+          {a + b + c, nb}
+        end
+      end)
+
+    res
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,21 +3,30 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @input "1 1396"
+  @input "199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263"
 
   @tag :skip
   test "part1" do
     input = @input
     result = part1(input)
 
-    assert result
+    assert result == 7
   end
 
-  @tag :skip
+  @tag :skip_no
   test "part2" do
     input = @input
     result = part2(input)
 
-    assert result
+    assert result == 5
   end
 end
